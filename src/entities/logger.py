cbb612e9832d9f96817e040896dbb53891831aec
Roman Kolerov <coder8080@yandex.ru>
2025-03-18 17:03:14 +0300

    Use detailed format for logger

--- src/entities/logger.py
+++ src/entities/logger.py
@@ -11,6 +11,13 @@ console_handler.setLevel(logging.DEBUG)
 debug_file.setLevel(logging.DEBUG)
 error_file.setLevel(logging.ERROR)
 
+detailed_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - \
+[%(pathname)s:%(lineno)d] - %(message)s')
+
+console_handler.setFormatter(detailed_format)
+debug_file.setFormatter(detailed_format)
+error_file.setFormatter(detailed_format)
+
 logger.addHandler(console_handler)
 logger.addHandler(debug_file)
 logger.addHandler(error_file)
