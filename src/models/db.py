6de925e57243639bd9e1b9119a4c1c2d677e023b
Roman Kolerov <coder8080@yandex.ru>
2025-03-20 12:23:45 +0300

    Use psycopg

--- src/models/db.py
+++ src/models/db.py
@@ -1,11 +1,11 @@
 import peewee_async
 from entities import POSTGRES_USER, POSTGRES_PASSWORD
 
-db = peewee_async.PooledPostgresqlDatabase('usmile',
-                                           user=POSTGRES_USER,
-                                           password=POSTGRES_PASSWORD,
-                                           host="postgres",
-                                           port=5432)
+db = peewee_async.PsycopgDatabase('usmile',
+                                  user=POSTGRES_USER,
+                                  password=POSTGRES_PASSWORD,
+                                  host="postgres",
+                                  port=5432)
 
 
 class BaseModel(peewee_async.AioModel):
