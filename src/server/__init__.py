b126a0e8cb49245d3d84b6b5081680b5e8925991
Roman Kolerov <coder8080@yandex.ru>
2025-04-09 18:19:38 +0300

    Configure Liveness and Startup probes for kubernets

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -9,9 +9,14 @@ async def podname(request: web.Request):
     return web.json_response({"podname": POD_NAME})
 
 
+async def liveness(_):
+    return web.Response(status=200)
+
+
 async def start_site():
     app = web.Application()
     app.router.add_get("/podname", podname)
+    app.router.add_get("/liveness", liveness)
 
     runner = web.AppRunner(app)
     await runner.setup()
